032d2ccf3c0ab5c92b376b037b8df1d74d720ce9
BenjaminDeschamps <deschampsbenjamin71@gmail.com>
2023-03-31 08:23:52 +0200

    add class client

--- backend/api/client.py
+++ backend/api/client.py
@@ -1,9 +1,21 @@
 class Client:
-    _id = 0
-    _name = ""
-    _pseudo = ""
+    id = 0
+    pseudo = ""
+    password = ""
 
-    def __init__(name, pseudo):
-        self._id += 1
-        self._name = name
-        self._pseudo = pseudo
+    def __init__(self, pseudo, password):
+        self.id += 1
+        self.pseudo = pseudo
+        self.password = password
+
+    def get_id(self):
+        return self.id
+
+    def get_name(self):
+        return self.name
+
+    def get_pseudo(self):
+        return self.pseudo
+
+    def get_password(self):
+        return self.password
